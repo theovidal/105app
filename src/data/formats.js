43fa867126c331a3096587f5d0062f63f6abf794
Exybore <exybore@becauseofprog.fr>
2020-03-29 21:33:05 +0200

    feat: add formats and subjects list

---
+++ src/data/formats.js
@@ -0,0 +1,16 @@
+const formats = {
+  pdf: {
+    name: 'PDF',
+    icon: 'mdi-file-pdf-outline'
+  },
+  docx: {
+    name: 'Microsoft Word',
+    icon: 'mdi-file-word-outline'
+  },
+  xmind: {
+    name: 'Carte XMind',
+    icon: 'mdi-map-outline'
+  }
+}
+
+export default formats
--- src/data/subjects.js
+++ src/data/subjects.js
@@ -1,10 +1,64 @@
 const subjects = [
   {
+    name: 'Français',
+    slug: 'francais',
+    icon: 'mdi-book-open-page-variant',
+    color: [182, 61, 61]
+  },
+  {
+    name: 'Histoire',
+    slug: 'histoire',
+    icon: 'mdi-timer-sand',
+    color: [182, 119, 61]
+  },
+  {
+    name: 'Géographie',
+    slug: 'geographie',
+    icon: 'mdi-earth',
+    color: [61, 182, 122]
+  },
+  {
+    name: 'SVT',
+    slug: 'svt',
+    icon: 'mdi-sprout-outline',
+    color: [76, 182, 61]
+  },
+  {
     name: 'Physique Tronc Commun',
     slug: 'physique-tc',
     icon: 'mdi-atom-variant',
-    color: '#5E3DB6'
-  }
+    color: [94, 91, 182]
+  },
+  {
+    name: 'Anglais',
+    slug: 'anglais',
+    icon: 'mdi-translate',
+    color: [182, 61, 136]
+  },
+  {
+    name: 'Espagnol',
+    slug: 'espagnol',
+    icon: 'mdi-translate',
+    color: [221, 146, 24]
+  },
+  {
+    name: 'Mathématiques',
+    slug: 'maths',
+    icon: 'mdi-chart-line',
+    color: [178, 182, 61]
+  },
+  {
+    name: 'Physique-Chimie',
+    slug: 'physique-chimie',
+    icon: 'mdi-beaker-outline',
+    color: [119, 61, 182]
+  },
+  {
+    name: "Sciences de l'ingénieur",
+    slug: 'si',
+    icon: 'mdi-engine',
+    color: [63, 61, 182]
+  },
 ]
 
 export default subjects
