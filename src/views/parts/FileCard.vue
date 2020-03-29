fea72399878fbde26d54f9c21aee713b05c84b67
Exybore <exybore@becauseofprog.fr>
2020-03-29 21:37:25 +0200

    feat: add file card component

--- src/views/parts/FileCard.vue
+++ src/views/parts/FileCard.vue
@@ -1,45 +1,48 @@
 <template>
   <v-card
-    :to="link">
+    :to="`/subjects/${subject.slug}/${file.slug}`">
     <v-card-text>
-      <p class="display-1 text--primary">{{ title }}</p>
-      <p v-if="subtitle !== ''">
+      <p class="display-1 text--primary">{{ file.name }}</p>
+      <p v-if="displaySubject">
         <v-icon
-          v-if="icon !== ''"
+          :color="getRgba(subject.color)"
           left>
-          {{ icon }}
+          {{ subject.icon }}
         </v-icon>
-        {{ subtitle }}
+        {{ subject.name }}
       </p>
-      <div class="text--primary">{{ description }}</div>
+      <div
+        class="text--primary"
+        v-html="file.description"/>
+      <v-chip>{{ dateToText(file.added) }}</v-chip>&nbsp;
+      <v-chip>{{ file.level }}</v-chip>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import { getRgba } from '@/utils/color'
+import { dateToText } from '@/utils/parsing'
+
 export default {
   name: 'FileCard',
   props: {
-    title: {
-      type: String,
+    file: {
+      type: Object,
       required: true
     },
-    subtitle: {
-      type: String,
-      default: ''
-    },
-    icon: {
-      type: String,
-      default: ''
-    },
-    description: {
-      type: String,
+    subject: {
+      type: Object,
       required: true
     },
-    link: {
-      type: String,
-      required: true
+    displaySubject: {
+      type: Boolean,
+      default: false
     }
+  },
+  methods: {
+    dateToText,
+    getRgba
   }
 }
 </script>
