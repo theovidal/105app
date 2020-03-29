fbefd76ed26ad2393888703a97cd887d3fb18497
Exybore <exybore@becauseofprog.fr>
2020-03-29 21:36:33 +0200

    feat: enrich files store

--- src/store/modules/files.js
+++ src/store/modules/files.js
@@ -1,14 +1,47 @@
-import files from '../../data/files'
+import { loadFiles } from '../../utils/storage'
 
 const state = {
-  files
+  files: loadFiles()
 }
 const getters = {
-  getFilesBySubject: state => subject => {
-    return state.files.filter(file => file.subject === subject)
+  getAllFiles: state => {
+    let files = []
+    for (let subject in state.files) {
+      files = [
+        ...files,
+        ...state.files[subject]
+      ]
+    }
+    return files
   },
-  getFileBySlug: state => slug => {
-    return state.files.find(file => file.slug === slug)
+  getFilesBySubject: state => subject => {
+    let files = state.files[subject]
+    if (files === undefined) {
+      return []
+    } else {
+      return files
+    }
+  },
+  getFileBySlug: state => (subject, slug) => {
+    return state.files[subject].find(file => file.slug === slug)
+  },
+  getLastFiles: (state, getters) => {
+    let files = getters.getAllFiles.sort((a,b) => {
+      const dateA = new Date(a.added).getTime()
+      const dateB = new Date(b.added).getTime()
+      return (dateA > dateB) ? -1 : ((dateB > dateA) ? 1 : 0)
+    })
+    return files.slice(0, 5)
+  },
+  searchFiles: (state, getters) => (query, subjects) => {
+    query = query.toLowerCase()
+    return getters.getAllFiles.filter(file => {
+      let name = file.name.toLowerCase()
+      let description = file.description.toLowerCase()
+
+      let matched = name.match(query) !== null || description.match(query) !== null
+      return matched && subjects.includes(file.subject)
+    })
   }
 }
 const mutations = {}
