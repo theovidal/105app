1ba3d5d6386cade713572efc32d902d0da12d0bc
Exybore <exybore@becauseofprog.fr>
2020-04-11 21:37:51 +0200

    feat: add file suggestions for same subject

---
+++ src/views/parts/FilesSlider.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-row style="flex-wrap: nowrap; overflow-x: scroll">
+    <v-col
+      v-for="file in files"
+      :key="file.slug"
+      cols="11"
+      md="4"
+      sm="6"
+      lg="3">
+      <file-card
+        :file="file"
+        :subject="getSubjectBySlug(file.subject)"
+        :display-subject="displaySubject"/>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import FileCard from './FileCard'
+
+export default {
+  name: 'FilesSlider',
+  components: { FileCard },
+  props: {
+    files: {
+      type: Array,
+      required: true
+    },
+    displaySubject: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    ...mapGetters(['getSubjectBySlug'])
+  }
+}
+</script>
